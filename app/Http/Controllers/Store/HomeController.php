53c36ab1a4de18e3f8b73ad6ad3ecfe7151f2174
Ajay Kumar Pradhan <ajaykpradhan61@gmail.com>
2017-10-26 16:47:00 +0530

    added dynamic categories and sub-categories in header section

---
+++ app/Http/Controllers/Store/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Route;
+use Session;
+use App\Category;
+use App\SubCategory;
+use App\Product;
+use App\ProductImage;
+use App\Attribute;
+
+class HomeController extends Controller
+{
+	public function getHome(Request $request) {
+		$categories = Category::with(['sub_categories' => function($query) { $query->where('status', 'ACTIVE')->orderBy('id', 'asc'); }])->where('status', 'ACTIVE')->orderBy('id', 'asc')->get();
+
+		return view('store.home', [ 'categories' => $categories ]);
+	}
+}
--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('store.app')
 
 @section('content')
 <div class="body-content outer-top-xs" id="top-banner-and-menu">

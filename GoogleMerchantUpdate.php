ae906dcf3b5fc735b6b9e855c4e9040d687480d1
Ajay Pradhan <ajay@rorko.com>
2017-11-14 05:26:02 +0530

    adding scheduler

--- GoogleMerchantUpdate.php
+++ GoogleMerchantUpdate.php
@@ -1,13 +1,43 @@
 <?php
 class GoogleMerchentUpdate {
-    public static function run()
-    {
-    	$shopping_content = Google::make('ShoppingContent');
+	public static function run() {
+		echo "It's running.";
+		/*
+		try {
+			$seller_products = SellerProduct::where('status', 'ACTIVE')->whereHas('product', function($query) {
+				$query->where('status', 'ACTIVE');
+			})->whereHas('product.product_images', function($query) {
+				$query->where('status', 'ACTIVE');
+			})->with([ 'product' => function($query) {
+				$query->where('status', 'ACTIVE');
+			}, 'product.product_images' => function($query) {
+				$query->where('status', 'ACTIVE');
+			} ])->whereHas('product.category', function($query) {
+				$query->where('status', 'ACTIVE');
+			})->whereHas('product.sub_category', function($query) {
+				$query->where('status', 'ACTIVE');
+			})->orderBy('id', 'asc')->get();
 
-		print_r($shopping_content->products->listProducts('114635174'));
+			$feedXmlStr = "";
+			$feedXmlStr = $feedXmlStr . "<?xml version=\"1.0\"?>";
+			$feedXmlStr = $feedXmlStr . "<rss xmlns:g=\"http://base.google.com/ns/1.0\" version=\"2.0\">";
 
-    	echo "It's working.";
-    }
+			$feedXmlStr = $feedXmlStr . "\n\t<channel>";
+			$feedXmlStr = $feedXmlStr . "\n\t\t<title>GBMart</title>";
+			$feedXmlStr = $feedXmlStr . "\n\t\t<link>https://www.gbmart.in</link>";
+			$feedXmlStr = $feedXmlStr . "\n\t\t<description>This is a sample feed containing the required and recommended attributes for a variety of different products</description>";
+			
+			$feedXmlStr = $feedXmlStr . "\n\t</channel>";
+			$feedXmlStr = $feedXmlStr . "\n</rss>";
+
+			\File::put(public_path() . "/feeds.xml", $feedXmlStr);
+			echo (public_path() . "/feeds.xml");
+		}
+		catch (Exception $ex) {
+			echo ("Error occurred.");
+		}
+		*/
+	}
 }
 GoogleMerchentUpdate::run();
 ?>
